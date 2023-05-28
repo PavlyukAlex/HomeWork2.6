be6968102117c4a7fbf6b3c77962c95409cfa4c4
PavlyukAlex <134655916+PavlyukAlex@users.noreply.github.com>
2023-05-28 09:36:16 +0200

    Initial Commit

---
+++ HomeWork2.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.6
+//
+//  Created by Oleksiy Pavlyuk on 28/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
